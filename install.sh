63fd275dec94c5d92b748633c23096c2b3be9f9a
Thomas Bereknyei <tomberek@gmail.com>
2015-07-31 21:02:43 -0400

    Converting to stack usage.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ verlte() {
   [ "$1" = `echo -e "$1\n$2" | sort -t '.' -k 1,1n -k 2,2n -k 3,3n -k 4,4n | head -n1` ]
 }
 
-for i in ctags git ghc cabal make vim curl-config happy; do
+for i in ctags git ghc make vim happy; do
   command -v $i >/dev/null
   if [ $? -ne 0 ] ; then
     msg "Installer requires ${i}. Please install $i and try again."
@@ -14,24 +14,28 @@ for i in ctags git ghc cabal make vim curl-config happy; do
   fi
 done
 
-CABAL_VER=$(cabal --numeric-version)
 VIM_VER=$(vim --version | sed -n 's/^.*IMproved \([^ ]*\).*$/\1/p')
 GHC_VER=$(ghc --numeric-version)
+
+msg "Updating stack package list"
+stack update
 
 if ! verlte '7.4' $VIM_VER ; then
   msg "Vim version 7.4 or later is required. Aborting."
   exit 1
 fi
 
-if ! verlte '1.18' $CABAL_VER ; then
-  msg "Cabal version 1.18 or later is required. Aborting."
+if ! verlte '7.8.4' $GHC_VER ; then
+  msg "GHC version 7.8.4 or later is required. Aborting."
   exit 1
 fi
-
-if ! verlte '7.6.3' $GHC_VER ; then
-  msg "GHC version 7.6.3 or later is required. Aborting."
-  exit 1
-fi
+for i in ghc-mod hasktags codex hscope pointfree pointful hoogle stylish-haskell; do
+  command -v $i >/dev/null
+  if [ $? -ne 0 ] ; then
+    msg "Installer requires ${i}. Attempting to use stack. (Note: ghc-mod, hoogle and hscope may require ghc-7.8.4)"
+    stack install $i --prefetch
+  fi
+done
 
 if ! ctags --version | grep -q "Exuberant" ; then
   msg "Requires exuberant ctags, not just ctags. Aborting."
@@ -78,59 +82,12 @@ vim -T dumb -E -u $endpath/.vimrc +PluginInstall! +PluginClean! +qall
 msg "Building vimproc.vim"
 make -C ~/.vim/bundle/vimproc.vim
 
-msg "Updating cabal package list"
-cabal update
-
 msg "Installing git-hscope"
 mkdir -p $endpath/bin
 cp $endpath/git-hscope $endpath/bin
 
-function create_stackage_sandbox {
-  msg "Initializing stackage sandbox in $dir"
-  dir=$1
-  cd $dir
-  cabal sandbox init
-
-  if verlte '7.10' $GHC_VER ; then
-    curl -L https://beta.stackage.org/nightly/cabal.config > cabal.config
-  else
-    curl -L https://www.stackage.org/lts/cabal.config > cabal.config
-  fi
-
-  cd -
-}
-
-function build_shared_binary {
-  dir=$1
-  pkg=$2
-  constraint=$3
-
-  if [ -e $endpath/bin/$pkg ]
-  then
-    msg "$pkg is already installed, skipping build"
-    return
-  fi
-
-  msg "Building $pkg (in $dir)"
-  cd $dir
-  cabal install -j --reorder-goals --disable-documentation --datadir=$endpath/data --force-reinstalls "${constraint:-$pkg}"
-
-  msg "Saving $pkg binaries"
-  mv .cabal-sandbox/bin/* $endpath/bin
-  cd -
-}
-
-sb=`mktemp -d ${TMPDIR:-/tmp}/build-XXXX`
-create_stackage_sandbox $sb
-
-for i in ghc-mod hasktags codex hscope pointfree pointful hoogle stylish-haskell; do
-  build_shared_binary $sb $i
-done
-
-rm -fr $sb
-
 msg "Building Hoogle database..."
-$endpath/bin/hoogle data
+hoogle data
 
 msg "Setting git to use fully-pathed vim for messages..."
 git config --global core.editor $(which vim)
